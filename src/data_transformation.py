a4392ac558f1ec682823c85ba5ebfc615623afcb
Harounnn <medharoun.1919@gmail.com>
2023-05-14 15:43:23 +0100

    Data Transformation DONE

--- src/data_transformation.py
+++ src/data_transformation.py
@@ -9,13 +9,26 @@ class DataTransformation():
     def __init__(self):
         pass
 
+    def add_epoch_column(self,year):
+        first_epoch = [1990,2000]
+        second_epoch = [2000,2010]
+        third_epoch = [2010,2015]
+        year = int(year)
+        
+        if year in range(first_epoch[0],first_epoch[1]+1):
+            return f'{first_epoch[0]} - {first_epoch[1]}'
+        elif year in range(second_epoch[0],second_epoch[1]+1):
+            return f'{second_epoch[0]} - {second_epoch[1]}'
+        else :
+            return f'{third_epoch[0]} - {third_epoch[1]}'
+
     def data_transformation(self):
         data_ingestion_obj = data_ingestion.DataIngestion()
 
         # Read the dataset 
         main_df = pd.read_csv(data_ingestion_obj.get_csv_files_paths()[0])
-        indicators_df = pd.read_csv(data_ingestion_obj.get_csv_files_paths()[1])
-        countries_df = pd.read_csv(data_ingestion_obj.get_csv_files_paths()[2])
+        countries_df = pd.read_csv(data_ingestion_obj.get_csv_files_paths()[1])
+        indicators_df = pd.read_csv(data_ingestion_obj.get_csv_files_paths()[2])
 
         #Transform Indicators dataframe
         education_indicators = []
@@ -25,8 +38,11 @@ class DataTransformation():
         education_indicators = np.array(education_indicators)
         education_indicators = list(np.unique(education_indicators))
 
-        indicators_df_filter = ( (indicators_df['Topic'] == ed_ind[0]) | (indicators_df['Topic'] == ed_ind[0]) | 
-                                 (indicators_df['Topic'] == ed_ind[0]) | (indicators_df['Topic'] == ed_ind[0]) )
+        indicators_df_filter = ( (indicators_df['Topic'] == education_indicators[0]) | 
+                                 (indicators_df['Topic'] == education_indicators[1]) | 
+                                 (indicators_df['Topic'] == education_indicators[2]) | 
+                                 (indicators_df['Topic'] == education_indicators[3]) )
+
         indicators_df = indicators_df[indicators_df_filter][['Topic','Indicator Name']]
 
         #Transform countries dataframe
@@ -61,10 +77,64 @@ class DataTransformation():
         df = pd.melt(df,id_vars=['Indicator Name','Country Code'],
                     value_vars=[str(i) for i in range(start_year,last_year + 1)]
                     ,var_name='Year', value_name='Value')
+
         df = df.dropna()
         df = df.merge(countries_df,left_on='Country Code', right_on='Country Code', how='left')
+
         df = df[['Indicator Name','Short Name','Country Code','Currency Unit','Region','Year','Value']]
 
-        #################################
-                #IN PROGRESS#
-        #################################
+        #Preparing dataframes for visualizatons
+
+        ## 1st dataframe named q1 as question 1
+        q1 = df.groupby(['Indicator Name','Region'])['Value'].mean()['DHS: Gross attendance rate. Post Secondary']
+        q1 = q1.to_frame().reset_index()
+        q1['Value'] = round(q1['Value'],1)
+
+        ## 2nd dataframe named q2 as question 2
+        for i,j in df.groupby('Indicator Name')[['Year','Value']]:
+            if i == 'DHS: Proportion of out-of-school. Primary. Female':
+                d1 = j
+            elif i == 'DHS: Proportion of out-of-school. Primary. Male':
+                d2 = j
+                
+        q21 = d1.groupby('Year').mean() #Female dataframe
+
+        q22 = d2.groupby('Year').mean() #Male dataframe
+
+        value_list_f = []
+        value_list_m = []
+        epoch_list = []
+
+        for j in range(6,31,5):
+            
+            i = j-6
+            
+            value_list_f.append(q21.reset_index().iloc[i:j]['Value'].mean())
+            value_list_m.append(q22.reset_index().iloc[i:j]['Value'].mean())
+            
+            epoch_list.append(str(i+1990)+'-'+str(j+1990-1))
+            
+        q2f = pd.DataFrame({'Value' : value_list_f,
+                            'Years':epoch_list,
+                            'Gender':'Female'})
+
+        q2m = pd.DataFrame({'Value' : value_list_m,
+                            'Years':epoch_list,
+                            'Gender':'Male'})
+
+        q2 = pd.concat([q2f,q2m])
+
+        q2['Value'] = round(q2['Value'], 2)
+
+        ## 3rd dataframe named q3 as question 3
+        mask = df['Indicator Name'] == 'DHS: Gross attendance rate. Post Secondary'
+        q3 = df[mask].groupby(['Country Code','Year']).mean(numeric_only=True).reset_index()
+        q3 = q3.merge(countries_df, on='Country Code').drop(['Currency Unit'], axis=1).sort_values('Year')
+
+        q3['Epoch'] = q3['Year'].apply(self.add_epoch_column)
+
+
+        #Return these 3 dataframes
+        return (q1,
+                q2,
+                q3)
