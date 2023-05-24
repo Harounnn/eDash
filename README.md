cda92cef2bcc93e3ab8135919069dffafd899331
Harounnn <medharoun.1919@gmail.com>
2023-05-25 00:55:20 +0100

    end of development

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# End to end Data analysis project
+# End-to-end Data analysis project
 ## Dataset
 [Dataset Link](https://www.kaggle.com/datasets/theworldbank/world-bank-intl-education?select=country_summary)
+
+I manually gathered data by searching a lot from google datasets to kaggle to so many different websites and I was thinking even to web-scraping . But , I found this dataset which is originally a BigQuery dataset but I made a decision to explore it through multiple csv files.
 
 For in-depth understanding of the indicators present in the dataset : Click [Here](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi13KXk0a_-AhUKtaQKHSZhASAQFnoECA8QAQ&url=https%3A%2F%2Fdata.unicef.org%2Fwp-content%2Fuploads%2F2019%2F07%2FMICS-EAGLE-implementation-guidebook_English.pdf&usg=AOvVaw3YFPolmWYUR0zdKrXJ-NIq)
 
 ## About the notebook
 Github don't render embedded HTML in notebooks so to see the visualizations that are present in the EDA notebook so I displayed it as PNG images but I still suggest downloading it locally and run it yourself if you are intrested in interactive plots , because at the moment the application is not yet done . When it is done , the visualizations will be available of course in the web-based dashboard.
 
-## Screenshot from the webapp (TEST)
+## Screenshot from the webapp 
 
 ![test](https://github.com/Harounnn/eDash/blob/3abf62d37f21669ad9655243db590fd609929677/screenshots/test1.png)
+![pic1](https://github.com/Harounnn/eDash/blob/3abf62d37f21669ad9655243db590fd609929677/screenshots/pic1.png)
+![pic2](https://github.com/Harounnn/eDash/blob/3abf62d37f21669ad9655243db590fd609929677/screenshots/pic2.png)
 
-## Work In Progress . . .
+
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 
 import src.data_transformation as dt
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data_frames = dt.DataTransformation().data_transformation()
 
@@ -12,12 +13,34 @@ figures = []
 start_year = 1990
 last_year = 2015
 fig = px.pie(data_frames[0], values='Value', names='Region', hole=0.05, hover_data={'Region':False},
-                             title=f'Mean Post Secandary GAR from {start_year} to {last_year}')
+                             title=f'Mean Post Secandary GAR from {start_year} to {last_year}',
+                             template='plotly_dark',color_discrete_sequence=px.colors.qualitative.Set3)
 figures.append(fig)
 
-app.layout = html.Div(children=[
-    html.H1(children='TESTING'),
-    dcc.Graph(figure=figures[0])])
+fig = px.bar(data_frames[1], x='Years', y='Value', color='Gender', barmode='group',
+             title='Proportion of out of school in Primary Education (Male and Female)',
+             hover_data={'Gender':False,'Years':False,'Value':False},
+             hover_name='Value', template='plotly_dark',color_discrete_sequence=px.colors.qualitative.Set3)
+
+figures.append(fig)
+
+fig = px.scatter_geo(data_frames[2], locations="Country Code", color="Region",
+                     title='GAR change through the years and over the globe',
+                     hover_name="Short Name", size="Value",
+                     projection="natural earth",animation_frame='Epoch', template='plotly_dark',
+                     color_discrete_sequence=px.colors.qualitative.Set3)
+
+figures.append(fig)
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([dcc.Graph(figure=figures[0])], width=6),
+        dbc.Col([dcc.Graph(figure=figures[2])], width=6)
+    ]),
+    dbc.Row([
+        dbc.Col([dcc.Graph(figure=figures[1])])
+    ])
+])
 
 
 if __name__ == "__main__":
